b3de592d3f4210493039ac8dc629efc5dce9d4da
Zane.XY <zane.xy@live.com>
2014-07-19 16:02:41 +0800

    rewrote form UI

--- data/get-text.js
+++ data/get-text.js
@@ -1,9 +1,11 @@
-var form = document.getElementById('text_capture');
-var textArea = document.getElementById("edit-box");
-var input = document.getElementById("headword");
+var form = document.getElementById('entryForm');
+var headwordField = document.getElementById("headword");
+var contextField = document.getElementById("context");
 
 self.port.on("show", function onShow(d) {
-  textArea.value = d;
+    if(d){
+      contextField.value = d;
+    }
 });
 
 if (form.attachEvent) {
@@ -14,14 +16,13 @@ if (form.attachEvent) {
 
 function processForm(e) {
     if (e.preventDefault) e.preventDefault();
-    text = textArea.value;
-    self.port.emit("text-entered", {
-        'title': input.value,
-        'content': text,
+    self.port.emit("entrySave", {
+        'headword': headwordField.value,
+        'context': contextField.value,
         'rating': 1
     });
-    input.value = '';
-    textArea.value = '';
+    headwordField.value = '';
+    contextField.value = '';
     // return false to prevent the default form behavior
     return false;
 }
